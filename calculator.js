7e877fbee834fff9fefbc045af82b0e5908ba581
dhdan97 <dhdan97@gmail.com>
2022-03-31 19:30:29 -0400

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,26 @@
+let divBtn=document.getElementById("div");
+let multBtn=document.getElementById("mult");
+let addBtn=document.getElementById("add");
+let subBtn=document.getElementById("minus");
+let dotBtn=document.getElementById("dot");
+let eqBtn=document.getElementById("equals");
+
+let n1=document.getElementById("one");
+let n2=document.getElementById("two");
+let n3=document.getElementById("three");
+let n4=document.getElementById("four");
+let n5=document.getElementById("five");
+let n6=document.getElementById("six");
+let n7=document.getElementById("seven");
+let n8=document.getElementById("eight");
+let n9=document.getElementById("nine");
+let n0=document.getElementById("zero");
+let textbox=document.getElementById("text-input");
+
+function Redfunc(){
+    document.querySelector('body').style.background=red;
+    textbox.value=5;
+   
+    }
+   
+    alert("hello");
